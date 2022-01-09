f127b14cd3b72198c50f1d70ecae4a99f19a27bb
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-08 20:29:23 -0600

    Update 4-new_in_list.py
    mhill44

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,1 +1,12 @@
-placeholder
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    elif element is None:
+        return my_list
+    else:
+        my_new_list = my_list.copy()
+        my_new_list[idx] = element
+        return my_new_list
