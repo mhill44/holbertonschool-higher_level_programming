bb461f1e0f5daf21273f81459d79820143fb45a6
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-08 20:40:57 -0600

    Update 8-multiple_returns.py
    mhill44

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,6 @@
-placeholder
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        return (len(sentence, None)), ""
+    else:
+        return (len(sentence), sentence[0])
