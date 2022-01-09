c4f6204c7df65d29ab742e7de82bc88f4b3e34e4
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-08 20:37:53 -0600

    Update 7-add_tuple.py
    mhill44

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,1 +1,23 @@
-placeholder
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) > 2 or len(tuple_b) > 2:
+        tuple_a = tuple_a[:2]
+        tuple_b = tuple_b[:2]
+
+    my_list = [tuple(iterable) for iterable in (tuple_a, tuple_b)]
+    if len(my_list) == 0:
+        return ()
+    while len(my_list) < 4:
+
+        if len(my_list[0]) > len(my_list[1]):
+            my_list[1] = my_list[1] + (0,) * \
+                (len(my_list[0]) - len(my_list[1]))
+
+        elif len(my_list[1]) > len(my_list[0]):
+            my_list[0] = my_list[0] + (0,) * \
+                (len(my_list[1]) - len(my_list[0]))
+
+        sum = (my_list[0][0] + my_list[1][0],
+               my_list[0][1] + my_list[1][1])
+
+        return(tuple(sum))
