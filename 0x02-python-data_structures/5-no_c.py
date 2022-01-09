732cdf2ca479ed96e19e961e4e2e8a094ae90688
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-08 20:30:06 -0600

    Update 5-no_c.py
    mhill44

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
-placeholder
+#!/usr/bin/python3
+def no_c(my_string):
+    my_array = []
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            my_array.append(i)
+    return ''.join(my_array)
