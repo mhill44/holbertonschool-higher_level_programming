83dc2fd54470db01db1bbdeb91136909fd96af22
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:45:33 -0600

    Update 0-square_matrix_simple.py
    mhill44

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-<placeholder>
+def square_matrix_simple(matrix=[]):
+    """
+    Return a new list with the square of each element of the matrix.
+    """
+    new_list = [[element**2 for element in row] for row in matrix]
+    return new_list
