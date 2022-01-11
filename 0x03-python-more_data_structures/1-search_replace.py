90cc29b8e6eb31da383a630440efb194b32fef2e
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:49:18 -0600

    Update 1-search_replace.py
    mhill44

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-<placeholder>
+def search_replace(my_list, search, replace):
+    """
+    Return a new list with the search replaced by the replace.
+    """
+    new_list = [replace if element == search else element
+                for element in my_list]
+    return new_list
