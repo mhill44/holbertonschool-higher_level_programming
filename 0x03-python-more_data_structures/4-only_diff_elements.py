cfcfcba3ae9cc078c52c0d297d82f4a4e0fdcc06
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:54:30 -0600

    Create 4-only_diff_elements.py
    mhill44

---
+++ 0x03-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """
+    Returns the elements that are not common between two sets
+    """
+    lambda_set_diff = \
+        set(map(lambda x: x, set_1)) | set(map(lambda x: x, set_2))
+    return lambda_set_diff - (set_1 & set_2)
