1b5edc86a4c99c19143120a9eb037d90e15dc7ee
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:59:21 -0600

    Update 9-multiply_by_2.py
    mhill44

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-<placeholder>
+def multiply_by_2(a_dictionary):
+    '''Multiplies each value in a dictionary by 2.
+    '''
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
