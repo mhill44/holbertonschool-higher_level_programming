f33aa71adc16270124bb7f7b8f0d6cbcf6e0ceac
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:51:14 -0600

    Update 2-uniq_add.py
    mhill44

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-<placeholder>
+def uniq_add(my_list=[]):
+    """
+    Return the sum of all unique elements in the list.
+    """
+    new_list = [element for element in my_list]
+    return sum(set(new_list))
