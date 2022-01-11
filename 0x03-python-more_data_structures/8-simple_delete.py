30504265716e2e04453951a2844581d7f2733a20
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-10 23:58:32 -0600

    Update 8-simple_delete.py
    mhill44

--- 0x03-python-more_data_structures/8-simple_delete.py
+++ 0x03-python-more_data_structures/8-simple_delete.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-<placeholder>
+def simple_delete(a_dictionary, key=""):
+    """Deletes a key in a dictionary.
+    """
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
