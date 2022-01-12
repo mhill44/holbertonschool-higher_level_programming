20c787fb992e9d5f56007d2adae61b57cc63fe83
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-11 22:17:00 -0600

    Update 4-main.py

--- 0x04-python-classes/4-main.py
+++ 0x04-python-classes/4-main.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
-<placeholder>
+Square = __import__('4-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
