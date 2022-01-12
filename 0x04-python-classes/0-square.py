4f09cb4d8eb6c77501341b897e579ae9dca57d78
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-11 21:44:47 -0600

    Update 0-square.py

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-<placeholder>
+
+class Square:
+    """A simple Square class that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
