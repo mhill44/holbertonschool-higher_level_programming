29756a417e0f9ed7c51b49f568aa1a8bfa438bc7
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-18 15:45:16 -0600

    Update 0-rectangle.py

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,71 +1,33 @@
 #!/usr/bin/python3
-""" Rectangle Class """
+""" A Rectangle Class """
 
 
 class Rectangle:
     """
-        Class Rectangle:
+        Initializes the rectangle.
+        width and `height must be integers and greater than or equal to 0.
+        `pep8` recommends to use `self.__width` instead of `self.width`.
+        Returns:
+            None
     """
     def __init__(self, width, height):
-        """
-            __init__ method:
-        """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        """
-            Property returns the width of the rectangle.
-            Returns:
-                The width of the rectangle
-        """
-        return self.__width
-
+        if self.width is not (type(int)):
+            raise TypeError("width must be an integer")
+        elif self.width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            return self.__width
 
     @property
     def height(self):
-        """
-            Property returns the height of the rectangle.
-            Returns:
-                The height of the rectangle
-        """
-        return self.__height
-
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            return (self.__width = value)
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
+        if self.height is not (type(int)):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        elif self.height < 0:
             raise ValueError("height must be >= 0")
         else:
-            return (self.__height = value)
-
-
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
-
-
-try:
-    my_rectangle = Rectangle(2, -3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    my_rectangle = Rectangle(-2, 3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+            return self.__height
