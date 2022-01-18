51813d34c645adb398aa6b692771c962e8418e6d
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-17 23:11:39 -0600

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
-<placeholder>
+
+"""
+This module contains: the add_integer function
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns: sum of a and b. Prints an error
+    message if values of a and b are not integers or floats
+    """
+    if type(a) == float:
+        a = int(a)
+    elif type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) == float:
+        b = int(b)
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
