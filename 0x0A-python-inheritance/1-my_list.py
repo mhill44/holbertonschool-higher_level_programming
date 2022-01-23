5572ec2142ac7ab3a18c8e515dbfd8ad07921071
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-01-23 13:26:02 -0600

    Create 1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My list class """
+
+
+class MyList(list):
+    """ My list class """
+    def __init__(self):
+        """ Init method """
+        super().__init__()
+
+    def print_sorted(self):
+        """ Prints the list in ascending order """
+        print(sorted(self))
