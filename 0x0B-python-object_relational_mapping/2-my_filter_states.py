3ecd9969c468856c7cce1377cf223508e9b6f06f
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-24 23:26:12 -0600

    Update 2-my_filter_states.py

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-"""takes an argument and displays all values
+'''takes an argument and displays all values
 in states table of hbtn_0e_0_usa
-where NAME matches argument"""
+where NAME matches argument'''
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    """ Connection Variables """
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = '3306'
+    A_STATE = sys.argv[4]
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    ''' Connect to the database '''
+    db = MySQLdb.connect(host=DB_HOST, port=int(DB_PORT),
+                         user=DB_USER, passwd=DB_PASS,
+                         db=DB_NAME, charset='utf8')
+
+    with db.cursor() as cur:
+        cur.execute(""" SELECT * FROM states
+                    WHERE states.name = %s""", (A_STATE,))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            if row[1] == sys.argv[4]:
+                print(row)
+
+        cur.close()
+        db.close()
+
