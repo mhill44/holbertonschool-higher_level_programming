469fb0a1fb3a15273e685d6f6bd477a0a35837bf
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-24 23:06:42 -0600

    Update 0-select_states.py

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Wll select states from the DB hbtn_0e_0_usa"""
+"""
+Wll select all from table states from the DB hbtn_0e_0_usa
+mapped to port 3306 on localhost
+"""
 
 if __name__ == '__main__':
 
@@ -9,8 +12,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
