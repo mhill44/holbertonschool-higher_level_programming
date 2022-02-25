35f8e8d63b9e13cf8407b00f3b2cb15bce7887cf
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-24 23:31:46 -0600

    Update 3-my_safe_filter_states.py

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,16 +5,29 @@ where name matches the argument
 and is safe from SQL injections"""
 
 if __name__ == '__main__':
+    
+import MySQLdb
+import sys
 
-    import MySQLdb
-    import sys
+    """ Connection Variables """
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = '3306'
+    A_STATE = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    ''' Connect to the database '''
+    db = MySQLdb.connect(host=DB_HOST, port=int(DB_PORT),
+                         user=DB_USER, passwd=DB_PASS,
+                         db=DB_NAME, charset='utf8')
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    with db.cursor() as cur:
+        cur.execute(""" SELECT * FROM states
+                    WHERE states.name = %(states)s""", {'states': A_STATE})
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+
+        cur.close()
+        db.close()
