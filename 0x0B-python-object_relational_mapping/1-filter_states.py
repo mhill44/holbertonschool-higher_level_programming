1154bc778f18ed94cc0b7c932b5c8805f0413cad
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-25 16:54:19 +0000

    Update 1-filter_states.py

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,14 @@ with uppercase N from the DB hbtn_0e_0_usa
 if __name__ == '__main__':
 
     import MySQLdb
-    import sys
+    ifrom sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute(""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC")
+    cursor.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
