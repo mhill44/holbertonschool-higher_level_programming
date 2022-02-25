779911f68d83c77643978614f130f749ee72285e
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-24 23:38:52 -0600

    Update 5-filter_cities.py

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -2,22 +2,36 @@
 """takes the name of a state as an arg and
 lists * cities of that state,
 using the database hbtn_0e_4_usa"""
+import MySQLdb
+import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    import MySQLdb
-    import sys
+    """ Connection Variables """
+    DB_HOST = 'localhost'
+    DB_USER = sys.argv[1]
+    DB_PASS = sys.argv[2]
+    DB_NAME = sys.argv[3]
+    DB_PORT = '3306'
+    A_STATE = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    ''' Connect to the database '''
+    db = MySQLdb.connect(host=DB_HOST, port=int(DB_PORT),
+                         user=DB_USER, passwd=DB_PASS,
+                         db=DB_NAME, charset='utf8')
 
     cur = db.cursor()
-    cur.execute("SELECT cities.name\
-                FROM cities LEFT JOIN states\
-                ON states.id = cities.state_id\
-                WHERE states.name = %s\
-                ORDER BY cities.id ASC", (sys.argv[4],))
-    rows = cur.fetchall()
-    print(", ".join([row[0] for row in rows]))
+
+    cur.execute("SELECT cities.name FROM cities\
+        JOIN states ON cities.state_id = states.id\
+            WHERE states.name=%s", (A_STATE, ))
+
+    query_rows = cur.fetchall()
+    out = []
+    for row in query_rows:
+        out.append(row[0])
+
+    print(', '.join(out))
+
     cur.close()
     db.close()
