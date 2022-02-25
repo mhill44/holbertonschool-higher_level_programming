364a7c9b71eb2c89025e784b6a50a733b207d9d6
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-25 16:56:26 +0000

    Update 1-filter_states.py

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ with uppercase N from the DB hbtn_0e_0_usa
 if __name__ == '__main__':
 
     import MySQLdb
-    ifrom sys import argv
+    from sys import argv
 
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
