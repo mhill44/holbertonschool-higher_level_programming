2522ed2f1d39a3d16ca20738c42af314fb081d4f
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-02-25 16:03:59 +0000

    Update 1-filter_states.py

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""
+'''
 a script that lists all states with a name starting
 with uppercase N from the DB hbtn_0e_0_usa
-"""
+'''
 
 if __name__ == '__main__':
 
@@ -13,8 +13,11 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    cursor.execute(SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+        
+        cursor.close()
+        db.close()
