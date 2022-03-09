2c24482afe87517fa482118dbb241fff9a00c455
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-03-08 18:54:43 -0600

    Create 0-hbtn_status.py

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A script that will fetch:  https://intranet.hbtn.io/status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+        req = urllib.request.Request('https://intranet.hbtn.io/status')
+        with urllib.request.urlopen(req) as response:
+                page = response.read()
+                print('Body response:')
+                print('\t- type: {}'.format(type(page)))
+                print('\t- content: {}'.format(page))
+                print('\t- utf8 content: {}'.format(page.decode('UTF-8')))
