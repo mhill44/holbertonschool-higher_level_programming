9b351b93966c681903820549477b52b3a8b8c0f2
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-03-08 19:24:17 -0600

    Create 8-json_api.py

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A Python script that 'takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter'
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    req = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        val = req.json()
+        if val == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(val.get("id"), val.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
