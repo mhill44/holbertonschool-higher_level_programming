82f7a5be80ea34a663be6e67671bcdf3fd251773
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-04-05 22:27:22 -0500

    Update 4-rectangle.js
    fine tuning script

--- 0x01-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,16 +2,22 @@
 /* A class named Rectangle that definies a rectangle */
 
 module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor (_w, _h) {
+    this.width = _w;
+    this.height = _h;
   }
-
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      console.log("X".repeat(this.width));
     }
+  }
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
   }
 };
