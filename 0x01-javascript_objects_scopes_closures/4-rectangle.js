2d6dd4ffc60645ce776d99210a14d12c1f2afd58
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-04-05 22:39:18 -0500

    Update 4-rectangle.js

--- 0x01-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
 /* A class named Rectangle that defs a rectangle */
-
 module.exports = class Rectangle {
-  constructor (_w, _h) {
-    this.width = _w;
-    this.height = _h;
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log("X".repeat(this.width));
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  rotate () {
-    let temp = this.width;
-    this.width = this.height;
-    this.height = temp;
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
   double () {
     this.width *= 2;
     this.height *= 2;
